e2ab71f93e8e421516368a090478c0710fe48c4f
Denis Iskandarov <d.iskandarov@gmail.com>
2024-08-31 14:11:38 +0400

    add excludes to indent-blankline

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -9,28 +9,27 @@ return {
   --   },
   -- }
   config = function()
-
     local highlight = {
-        "RainbowRed",
-        "RainbowYellow",
-        "RainbowBlue",
-        "RainbowOrange",
-        "RainbowGreen",
-        "RainbowViolet",
-        "RainbowCyan",
+      "RainbowRed",
+      "RainbowYellow",
+      "RainbowBlue",
+      "RainbowOrange",
+      "RainbowGreen",
+      "RainbowViolet",
+      "RainbowCyan",
     }
 
     local hooks = require("ibl.hooks")
     -- create the highlight groups in the highlight setup hook, so they are reset
     -- every time the colorscheme changes
     hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+      vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+      vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+      vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+      vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+      vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+      vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+      vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
     end)
 
     require("ibl").setup({
@@ -42,7 +41,37 @@ return {
         enabled = false,
         highlight = highlight,
       },
-    })
-  end
-}
 
+      exclude = {
+        filetypes = {
+          "startify",
+          "dashboard",
+          "dotooagenda",
+          "log",
+          "fugitive",
+          "gitcommit",
+          "packer",
+          "vimwiki",
+          "markdown",
+          "json",
+          "txt",
+          "vista",
+          "help",
+          "todoist",
+          "NvimTree",
+          "neo-tree",
+          "peekaboo",
+          "git",
+          "TelescopePrompt",
+          "undotree",
+          "flutterToolsOutline",
+          "", -- for all buffers without a file type
+        },
+        buftypes = {
+          "terminal",
+          "nofile",
+        },
+      },
+    })
+  end,
+}
