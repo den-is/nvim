eaceacfaa07aae506a49ed1aad69857169d378fa
Denis Iskandarov <d.iskandarov@gmail.com>
2024-08-31 17:51:24 +0400

    better desc for gitsigns key mappings

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -35,33 +35,33 @@ return {
         end, { desc = "Prev hunk" })
 
         -- Actions
-        map("n", "<leader>hs", gitsigns.stage_hunk, { desc = "Stage hunk" })
-        map("n", "<leader>hr", gitsigns.reset_hunk, { desc = "Reset hunk" })
+        map("n", "<leader>hs", gitsigns.stage_hunk, { desc = "Git - Stage hunk" })
+        map("n", "<leader>hr", gitsigns.reset_hunk, { desc = "Git - Reset hunk" })
         map("v", "<leader>hs", function()
           gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-        end, { desc = "Stage hunk" })
+        end, { desc = "Git - Stage hunk" })
         map("v", "<leader>hr", function()
           gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-        end, { desc = "Reset hunk" })
+        end, { desc = "Git - Reset hunk" })
 
-        map("n", "<leader>hS", gitsigns.stage_buffer, { desc = "Stage buffer" })
-        map("n", "<leader>hR", gitsigns.reset_buffer, { desc = "Reset buffer" })
+        map("n", "<leader>hS", gitsigns.stage_buffer, { desc = "Git - Stage buffer" })
+        map("n", "<leader>hR", gitsigns.reset_buffer, { desc = "Git - Reset buffer" })
 
-        map("n", "<leader>hu", gitsigns.undo_stage_hunk, { desc = "Undo stage hunk" })
-        map("n", "<leader>hp", gitsigns.preview_hunk, { desc = "Preview hunk" })
+        map("n", "<leader>hu", gitsigns.undo_stage_hunk, { desc = "Git - Undo stage hunk" })
+        map("n", "<leader>hp", gitsigns.preview_hunk, { desc = "Git - Preview hunk" })
 
         map("n", "<leader>hb", function()
           gitsigns.blame_line({ full = true })
         end, { desc = "Blame line" })
-        map("n", "<leader>hB", gitsigns.toggle_current_line_blame, { desc = "Toggle line blame" })
-        map("n", "<leader>hd", gitsigns.diffthis, { desc = "Diff this" })
+        map("n", "<leader>hB", gitsigns.toggle_current_line_blame, { desc = "Git - Toggle line blame" })
+        map("n", "<leader>hd", gitsigns.diffthis, { desc = "Git - Diff this" })
         map("n", "<leader>hD", function()
           gitsigns.diffthis("~")
         end, { desc = "Diff this ~" })
-        map("n", "<leader>td", gitsigns.toggle_deleted, { desc = "Toggle deleted" })
+        map("n", "<leader>td", gitsigns.toggle_deleted, { desc = "Git - Toggle deleted" })
 
         -- Text object
-        map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "Gitsigns select hunk" })
+        map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "Git - Select hunk" })
       end,
     })
   end,
