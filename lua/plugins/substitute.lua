4b6b1edcb88be1aa7fed6359e89aaaf8dceee87e
Denis Iskandarov <d.iskandarov@gmail.com>
2024-09-04 12:14:27 +0400

    substitute.nvim disabled for now

--- lua/plugins/substitute.lua
+++ lua/plugins/substitute.lua
@@ -1,14 +1,15 @@
 -- https://github.com/gbprod/substitute.nvim
+-- overrides default `s`, `S` keymaps. To review and play again later.
 return {
   "gbprod/substitute.nvim",
   event = { "BufReadPre", "BufNewFile" },
+  enabled = false,
   config = function()
     local substitute = require("substitute")
     substitute.setup()
-    -- new keymaps, overrides default vim/neovim shortcuts
-    vim.keymap.set("n", "s", substitute.operator, { desc = "Substitute with motion" })
-    vim.keymap.set("n", "ss", substitute.line, { desc = "Substitute line" })
-    vim.keymap.set("n", "S", substitute.eol, { desc = "Substitute to end of line" })
-    vim.keymap.set("x", "s", substitute.visual, { desc = "Substitute in visual mode" })
+    vim.keymap.set("n", "s", substitute.operator, { noremap = true, desc = "Substitute with motion" })
+    vim.keymap.set("n", "ss", substitute.line, { noremap = true, desc = "Substitute line" })
+    vim.keymap.set("n", "S", substitute.eol, { noremap = true, desc = "Substitute to end of line" })
+    vim.keymap.set("x", "s", substitute.visual, { noremap = true, desc = "Substitute in visual mode" })
   end,
 }
