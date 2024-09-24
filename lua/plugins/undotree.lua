1d06e5723a726e7994d11081eb743a082f539b2e
Denis Iskandarov <d.iskandarov@gmail.com>
2024-09-24 18:39:21 +0400

    add shortcut for untotree

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,3 +1,8 @@
+-- https://github.com/mbbill/undotree
 return {
   "mbbill/undotree",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    vim.keymap.set("n", "<leader>eu", vim.cmd.UndotreeToggle, { desc = "Open Undo Tree" })
+  end,
 }
