ae04bb71cb9bff16bb5ca2f608e3c7363d4cc065
Denis Iskandarov <d.iskandarov@gmail.com>
2024-10-01 20:40:29 +0400

    add comments for Comment.nvim

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,9 +1,14 @@
 -- https://github.com/numToStr/Comment.nvim
+-- "comment on a new line" continuation is disabled in config/autocmds.lua
+-- https://neovim.io/doc/user/various.html#_3.-commenting
+-- Basic shortcuts:
+-- gcc - line comment
+-- gbc - block commant
 return {
-  'numToStr/Comment.nvim',
+  "numToStr/Comment.nvim",
   event = { "BufReadPre", "BufNewFile" },
   dependencies = {
-    'JoosepAlviste/nvim-ts-context-commentstring',
+    "JoosepAlviste/nvim-ts-context-commentstring",
   },
   config = function()
     local comment = require("Comment")
