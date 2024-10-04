ad9e014fbe1eda1a72cffb3ed328ac76e3b7bb43
Denis Iskandarov <d.iskandarov@gmail.com>
2024-10-04 12:00:31 +0400

    verbose treesitter textobjects config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -18,7 +18,7 @@ return {
     require("nvim-treesitter.configs").setup({
       sync_install = false,
       highlight = { enable = true },
-      indent = { enable = true },
+      indent = { enable = true }, -- Indentation based on treesitter for the = operator. NOTE: This is an experimental feature.
       endwise = { enable = true },
       autopairs = { enable = true },
       -- A list of parser names, or "all" (the listed parsers MUST always be installed)
@@ -56,6 +56,114 @@ return {
         "toml",
         "vim",
         "yaml",
+      },
+
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          -- mappings for incremental selection (visual mappings)
+          -- set to `false` to disable one of the mappings
+          init_selection = "gnn", -- maps in normal mode to init the node/scope selection
+          node_incremental = "grn", -- increment to the upper named parent
+          scope_incremental = "grc", -- increment to the upper scope (as defined in locals.scm)
+          node_decremental = "grm", -- decrement to the previous node
+        },
+      },
+
+      textobjects = {
+
+        -- peek_definition_code:
+        -- show textobject surrounding definition as determined using Neovim's built-in LSP in a floating window.
+        -- Press the shortcut twice to enter the floating window.
+        lsp_interop = {
+          enable = true,
+          border = "none",
+          floating_preview_opts = {},
+          peek_definition_code = {
+            ["<leader>df"] = "@function.outer",
+            ["<leader>dF"] = "@class.outer",
+          },
+        },
+
+        keymaps = {
+          ["iL"] = {
+            -- you can define your own textobjects directly here
+            go = "(function_definition) @function",
+          },
+          -- or you use the queries from supported languages with textobjects.scm
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["aC"] = "@class.outer",
+          ["iC"] = "@class.inner",
+          ["ac"] = "@conditional.outer",
+          ["ic"] = "@conditional.inner",
+          ["ae"] = "@block.outer",
+          ["ie"] = "@block.inner",
+          ["al"] = "@loop.outer",
+          ["il"] = "@loop.inner",
+          ["is"] = "@statement.inner",
+          ["as"] = "@statement.outer",
+          ["ad"] = "@comment.outer",
+          ["am"] = "@call.outer",
+          ["im"] = "@call.inner",
+        },
+
+        select = {
+          enable = true,
+          -- Automatically jump forward to textobj, similar to targets.vim
+          lookahead = true,
+          -- If you set this to `true` (default is `false`) then any textobject is
+          -- extended to include preceding or succeeding whitespace. Succeeding
+          -- whitespace has priority in order to act similarly to eg the built-in `ap`.
+          include_surrounding_whitespace = true,
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+            -- Or you can define your own textobjects like this
+            ["iF"] = {
+              python = "(function_definition) @function",
+              cpp = "(function_definition) @function",
+              c = "(function_definition) @function",
+              java = "(method_declaration) @function",
+              go = "(method_declaration) @function",
+            },
+          },
+        },
+
+        move = {
+          enable = true,
+          set_jumps = true, -- whether to set jumps in the jumplist
+          goto_next_start = {
+            ["]m"] = "@function.outer",
+            ["]]"] = { query = "@class.outer", desc = "Next class start" },
+          },
+          goto_next_end = {
+            ["]M"] = "@function.outer",
+            ["]["] = "@class.outer",
+          },
+          goto_previous_start = {
+            ["[m"] = "@function.outer",
+            ["[["] = "@class.outer",
+          },
+          goto_previous_end = {
+            ["[M"] = "@function.outer",
+            ["[]"] = "@class.outer",
+          },
+        },
+
+        -- swap for example funtion args or params
+        swap = {
+          enable = true,
+          swap_next = {
+            ["<leader>a"] = "@parameter.inner",
+          },
+          swap_previous = {
+            ["<leader>A"] = "@parameter.inner",
+          },
+        },
       },
     })
 
