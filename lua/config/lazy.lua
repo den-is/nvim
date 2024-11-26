6961f47377dc2632cde8b16b74387ef4714b43c5
Denis Iskandarov <d.iskandarov@gmail.com>
2024-11-26 09:20:45 +0400

    update lazy config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,14 +1,16 @@
--- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -16,7 +18,13 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 
   spec = {
-    -- import your plugins from plugin directory
+    {
+      "LazyVim/LazyVim",
+      -- do not import the default plugins provided by Lazy.nvim
+      -- we use our own plugins
+      -- import = "lazyvim.plugins"
+    },
+    -- importing our plugins defined in "plugins" directory
     { import = "plugins" },
   },
 
@@ -34,5 +42,20 @@ require("lazy").setup({
   change_detection = {
     enabled = false,
     notify = true,
+  },
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
   },
 })
