4c8ff971e7fb8dde3c2b096bf5409ac9ea398297
Denis Iskandarov <d.iskandarov@gmail.com>
2024-12-13 12:19:48 +0400

    update cmp completions configuration

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -7,6 +7,7 @@
 -- https://github.com/L3MON4D3/LuaSnip
 --    https://github.com/rafamadriz/friendly-snippets
 -- https://github.com/onsails/lspkind.nvim
+-- https://www.reddit.com/r/neovim/comments/1bojtr0/please_share_your_nvimcmp_config/
 -- 'hrsh7th/cmp-nvim-lsp', -- added as a dependency in lsp.lua
 return {
   "hrsh7th/nvim-cmp",
@@ -25,6 +26,8 @@ return {
       build = "make install_jsregexp",
     },
     "saadparwaiz1/cmp_luasnip",
+    "hrsh7th/cmp-nvim-lua",
+    "ray-x/cmp-treesitter",
     "rafamadriz/friendly-snippets", -- usefull snippets VSCode like
     "onsails/lspkind.nvim", -- vscode-like pictograms for neovim lsp completion items
   },
@@ -33,17 +36,20 @@ return {
     local cmp = require("cmp")
     local luasnip = require("luasnip")
     local lspkind = require("lspkind")
-    local auto_select = true
+    local auto_select = false
 
     require("luasnip.loaders.from_vscode").lazy_load()
 
     cmp.setup({
 
       completion = {
+        -- https://neovim.io/doc/user/options.html#'completeopt'
         -- completeopt = "menu,menuone,preview,noselect",
-        completeopt = "menu,menuone,noinsert" .. (auto_select and "" or ",noselect"),
+        completeopt = "menu,menuone,preview" .. (auto_select and "" or ",noselect"),
       },
 
+      -- cmp always pops up and even if you don't want to select item, it will insert first item
+      -- preselect = cmp.PreselectMode.Item,
       preselect = auto_select and cmp.PreselectMode.Item or cmp.PreselectMode.None,
 
       snippet = {
@@ -53,13 +59,22 @@ return {
       },
 
       window = {
-        -- completion    = cmp.config.window.bordered(),
-        -- documentation = cmp.config.window.bordered(),
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
       },
+      -- view = {
+      --   entries = {
+      --     name = "custom",
+      --     selection_order = "near_cursor",
+      --     follow_cursor = true,
+      --   },
+      -- },
 
       mapping = cmp.mapping.preset.insert({
-        ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }), -- previous suggestion
-        ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }), -- next suggestion
+        ["<CR>"] = cmp.mapping.confirm({ select = auto_select, behavior = cmp.ConfirmBehavior.Insert }),
+        ["<C-e>"] = cmp.mapping.abort(), -- close completion window
+        ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
 
@@ -67,24 +82,35 @@ return {
         -- On MacOS <C-Space> is used by the system to change the input language. Should use different keybinding
         -- ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-S-x>"] = cmp.mapping.complete(),
-
-        ["<C-e>"] = cmp.mapping.abort(), -- close completion window
-        ["<CR>"] = cmp.mapping.confirm({ select = auto_select }),
       }),
 
       sources = cmp.config.sources({
         {
+          name = "luasnip",
+          group_index = 1,
+          option = { use_show_condition = true },
+        },
+        {
+          -- dependency added in lsp.lua
+          name = "nvim_lsp",
+          group_index = 2,
+        },
+        {
+          name = "treesitter",
+          keyword_length = 2,
+          group_index = 3,
+        },
+        {
           name = "path",
+          group_index = 4,
           option = {
             label_trailing_slash = true, -- Specify if directory names in the completion menu should include a trailing slash.
             trailing_slash = true, -- Specify if completed directory names should include a trailing slash.
           },
         },
-        { name = "nvim_lsp" }, -- dependency added in lsp.lua
-        { name = "luasnip" },
-        { name = "calc" },
         {
           name = "buffer",
+          group_index = 2,
           option = {
             get_bufnrs = function()
               --- to get only visible buffers
@@ -93,20 +119,40 @@ return {
               --   bufs[vim.api.nvim_win_get_buf(win)] = true
               -- end
               -- return vim.tbl_keys(bufs)
+              --
               --- to get all buffers
               return vim.api.nvim_list_bufs()
             end,
           },
         },
+        {
+          name = "lazydev",
+          group_index = 0,
+        },
+        {
+          name = "nvim_lua",
+          group_index = 4,
+        },
+        { name = "calc" },
       }),
-
       ---@diagnostic disable-next-line: missing-fields
       formatting = {
         -- fields = {},
         -- expandable_indicator = true,
         format = lspkind.cmp_format({
           maxwidth = 50,
-          ellipsis_char = "…",
+          -- ellipsis_char = "…",
+          ellipsis_char = "...",
+          menu = {
+            nvim_lsp = "[lsp]",
+            nvim_lua = "[lua]",
+            lazydev = "[lua]",
+            luasnip = "[snip]",
+            buffer = "[buf]",
+            path = "[path]",
+            calc = "[calc]",
+            treesitter = "[tree]",
+          },
         }),
       },
     })
