600175a3bf8ce96a930401c47961c84bb3bc407f
Denis Iskandarov <d.iskandarov@gmail.com>
2025-01-05 12:44:21 +0400

    update comment.lua

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,9 +1,10 @@
 -- https://github.com/numToStr/Comment.nvim
+-- https://github.com/JoosepAlviste/nvim-ts-context-commentstring
 -- "comment on a new line" continuation is disabled in config/autocmds.lua
 -- https://neovim.io/doc/user/various.html#_3.-commenting
 -- Basic shortcuts:
 -- gcc - line comment
--- gbc - block commant
+-- gbc - block comment
 return {
   "numToStr/Comment.nvim",
   event = { "BufReadPre", "BufNewFile" },
@@ -13,6 +14,8 @@ return {
   config = function()
     local comment = require("Comment")
     local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+
+    ---@diagnostic disable-next-line: missing-fields
     comment.setup({
       pre_hook = ts_context_commentstring.create_pre_hook(),
     })
