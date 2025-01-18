67f0f3563970ab41f7ef64df25d6a13ecc54ea5b
Denis Iskandarov <d.iskandarov@gmail.com>
2025-01-18 14:16:34 +0400

    switch mode to buffers instead of tabs

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,11 +1,16 @@
 -- https://github.com/akinsho/bufferline.nvim
+-- :h bufferline-configuration
 return {
   "akinsho/bufferline.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   version = "*",
   opts = {
     options = {
-      mode = "tabs",
+      -- mode = "tabs",
+      mode = "buffers",
+      close_command = "bdelete! %d",
+      show_tab_indicators = false,
+
       -- numbers = "both",
       offsets = {
         {
