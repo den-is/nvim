68e3452584718d537ecbac82b70c0114b2bdf182
Denis Iskandarov <d.iskandarov@gmail.com>
2025-04-06 13:43:38 +0400

    snacks - dashboard config

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,7 +2,6 @@
 return {
   "folke/snacks.nvim",
   enabled = true,
-  -- event = "VimEnter",
   priority = 1000,
   lazy = false,
   config = function()
@@ -54,7 +53,6 @@ return {
       quickfile = { enabled = true },
       statuscolumn = { enabled = true },
       words = { enabled = true },
-      dashboard = { enabled = true },
       indent = {
         enabled = true,
         indent = { only_scope = true }, -- only show indent where cursor is
@@ -64,6 +62,31 @@ return {
       notifier = {
         enabled = true,
         timeout = 2000,
+      },
+      dashboard = {
+        enabled = true,
+        preset = {
+          ---@type snacks.dashboard.Item[]
+          -- stylua: ignore start
+          pick = nil,
+          keys = {
+            -- { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.picker.smart({filter = {cwd = true}, layout = 'default'})" },
+            -- { icon = " ", key = "s", desc = "Find Text", action = ":lua Snacks.picker.grep({layout = 'default'})" },
+            { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+            { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+            { icon = " ", key = "b", desc = "File browser", action = function()  require("yazi").yazi(nil, vim.fn.getcwd()) end},
+            { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy check", enabled = package.loaded.lazy },
+            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+            -- stylua: ignore end
+          },
+        },
+        sections = {
+          { section = "header" },
+          { section = "keys", padding = { 1, 0 } },
+          { title = "Recent Files", section = "recent_files", limit = 8, cwd = true, padding = 1 },
+          { section = "startup" },
+        },
       },
     })
   end,
