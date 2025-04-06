5e0cfa3b6ce1d93ccb29416a9e323b90c9a9603b
Denis Iskandarov <d.iskandarov@gmail.com>
2025-04-06 13:43:10 +0400

    cleanup go.lua

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,38 +1,24 @@
 -- https://github.com/ray-x/go.nvim
 return {
-  {
-    "ray-x/go.nvim",
-    dependencies = {
-      "ray-x/guihua.lua",
-      "neovim/nvim-lspconfig",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    ft = { "go", "gomod" },
-    event = { "CmdlineEnter" },
-    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-    config = function()
-      require("go").setup({
-        lsp_cfg = false,
-        null_ls = false,
-        luasnip = false,
-        diagnostic = false,
-        lsp_inlay_hints = {
-          -- configured in `neovim/nvim-lspconfig` (lsp.lua)
-          -- <leader>i to toggle hints
-          enable = false,
-        },
-      })
-    end,
+  "ray-x/go.nvim",
+  dependencies = {
+    "ray-x/guihua.lua",
+    "neovim/nvim-lspconfig",
+    "nvim-treesitter/nvim-treesitter",
   },
-  -- https://github.com/olexsmir/gopher.nvim
-  {
-    "olexsmir/gopher.nvim",
-    enabled = false,
-    ft = "go",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    opts = {},
-  },
+  ft = { "go", "gomod" },
+  event = { "CmdlineEnter" },
+  build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  config = function()
+    require("go").setup({
+      lsp_cfg = false,
+      luasnip = false,
+      diagnostic = false,
+      lsp_inlay_hints = {
+        -- configured in `neovim/nvim-lspconfig` (lsp.lua)
+        -- <leader>i to toggle hints
+        enable = false,
+      },
+    })
+  end,
 }
