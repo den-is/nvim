8f412c327abd60cc663e7e8b53bc2cf6b1ea0bcb
Denis Iskandarov <d.iskandarov@gmail.com>
2025-04-27 12:23:02 +0400

    snacks dashboard lua format

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -70,20 +70,21 @@ return {
     dashboard = {
       enabled = true,
       preset = {
-          ---@type snacks.dashboard.Item[]
+        ---@type snacks.dashboard.Item[]
+        pick = nil, -- for now using Telescope
+        keys = {
           -- stylua: ignore start
-          pick = nil,
-          keys = {
-            -- { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.picker.smart({filter = {cwd = true}, layout = 'default'})" },
-            -- { icon = " ", key = "s", desc = "Find Text", action = ":lua Snacks.picker.grep({layout = 'default'})" },
-            { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-            { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-            { icon = " ", key = "b", desc = "File browser", action = function()  require("yazi").yazi(nil, vim.fn.getcwd()) end},
-            { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy check", enabled = package.loaded.lazy },
-            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-            -- stylua: ignore end
-          },
+          -- for now using Telescope instead of Snacks.picker
+          -- { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.picker.smart({filter = {cwd = true}, layout = 'default'})" },
+          -- { icon = " ", key = "s", desc = "Find Text", action = ":lua Snacks.picker.grep({layout = 'default'})" },
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "b", desc = "File browser", action = function()  require("yazi").yazi(nil, vim.fn.getcwd()) end},
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy check", enabled = package.loaded.lazy },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          -- stylua: ignore end
+        },
       },
       sections = {
         { section = "header" },
