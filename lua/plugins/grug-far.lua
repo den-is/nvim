384c3ff857d39c7991b6c444ad9ca23464e480d1
Denis Iskandarov <d.iskandarov@gmail.com>
2025-06-02 08:35:04 +0400

    grug-far config

--- lua/plugins/grug-far.lua
+++ lua/plugins/grug-far.lua
@@ -2,9 +2,25 @@
 return {
   "MagicDuck/grug-far.nvim",
   event = "VeryLazy",
-  config = function()
-    require("grug-far").setup({
-      engine = "ripgrep",
-    })
-  end,
+  cmd = "GrugFar",
+  keys = {
+    {
+      "<leader>sr",
+      function()
+        local grug = require("grug-far")
+        local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+        grug.open({
+          transient = true,
+          prefills = {
+            filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+          },
+        })
+      end,
+      mode = { "n", "v" },
+      desc = "Search and Replace",
+    },
+  },
+  opts = {
+    engine = "ripgrep",
+  },
 }
