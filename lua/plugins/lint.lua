916549e13a466cf586b07fe353cfc40c53b9148f
Denis Iskandarov <d.iskandarov@gmail.com>
2025-07-27 19:10:19 +0400

    liting <leader>xl instead of <leader>l

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -32,7 +32,7 @@ return {
       end,
     })
 
-    vim.keymap.set("n", "<leader>l", function()
+    vim.keymap.set("n", "<leader>xl", function()
       lint.try_lint()
     end, { desc = "Trigger linting for current file" })
   end,
