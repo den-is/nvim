5e5fc0a4f36dabcb6c94699374652d6e48faf94b
Denis Iskandarov <d.iskandarov@gmail.com>
2025-08-03 10:50:59 +0400

    optimze autocmds and filetypes setup

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,15 +3,27 @@ local api = vim.api
 vim.filetype.add({
   extension = {
     env = "sh",
+    -- https://github.com/hashicorp/terraform-ls/blob/main/docs/USAGE.md
+    -- expects a terraform filetype and not a tf filetype
+    tf = "terraform",
+    -- Detected automatically as terraform-vars filetype (probably integrated in terraform-ls)
+    -- can't add `terraform-vars` to conform.nvim formatters_by_ft table for autoformatting
+    tfvars = "hcl",
+    tfrc = "hcl",
   },
   filename = {
     [".env"] = "sh",
     ["tsconfig.json"] = "jsonc",
     [".yamlfmt"] = "yaml",
     [".yamllint"] = "yaml",
+    [".terraformrc"] = "hcl",
+    ["terraform.rc"] = "hcl",
+    [".tofurc"] = "hcl",
+    ["tofu.rc"] = "hcl",
   },
   pattern = {
     ["%.env%.[%w_.-]+"] = "sh",
+    [".*%.conf%.local"] = "conf",
   },
 })
 
@@ -23,7 +35,7 @@ local TrimWhiteSpaceGrp = api.nvim_create_augroup("TrimWhiteSpaceGrp", { clear =
 --   desc = "Trim trailing whitespace for all filetypes",
 -- })
 api.nvim_create_autocmd("BufWritePre", {
-  desc = "Trim trailing whitespace for all filetypes (with exceptions)",
+  desc = "Trim trailing whitespace for all filetypes except markdown",
   group = TrimWhiteSpaceGrp,
   callback = function()
     if vim.bo.filetype ~= "markdown" then
@@ -34,54 +46,18 @@ api.nvim_create_autocmd("BufWritePre", {
 
 -- Disable new line comment
 api.nvim_create_autocmd("BufEnter", {
-  desc = "Disable New Line comment",
+  desc = "Disable new line comments",
   callback = function()
     vim.opt.formatoptions:remove({ "c", "r", "o" })
   end,
 })
 
--- https://github.com/hashicorp/terraform-ls/blob/main/docs/USAGE.md
--- expects a terraform filetype and not a tf filetype
-api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  desc = "Detect terraform filetype",
-  pattern = { "*.tf" },
-  callback = function()
-    vim.api.nvim_command("set filetype=terraform")
-  end,
-})
-
--- Detected automatically as terraform-vars filetype (probably integrated in terraform-ls)
--- can't add `terraform-vars` to conform.nvim formatters_by_ft table for autoformatting
-api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  desc = "Detect terraform vars",
-  pattern = "*.tfvars",
-  callback = function()
-    vim.api.nvim_command("set filetype=hcl")
-  end,
-})
-
-api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  desc = "Set terraformrc filetype to hcl",
-  pattern = { ".terraformrc", "terraform.rc", ".tofurc", "tofu.rc", "*.tfrc" },
-  callback = function()
-    vim.api.nvim_command("set filetype=hcl")
-  end,
-})
-
-api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  desc = "Fix terraform and hcl comment string",
+api.nvim_create_autocmd("FileType", {
+  desc = "Fix Terraform and HCL comment string",
   group = vim.api.nvim_create_augroup("FixTerraformCommentString", { clear = true }),
-  pattern = { "*tf", "*.hcl", "*.tfvars", ".terraformrc", "terraform.rc", ".tofurc", "tofu.rc", "*.tfrc" },
+  pattern = { "hcl", "terraform", "terraform-vars" },
   callback = function(ev)
     vim.bo[ev.buf].commentstring = "# %s"
-  end,
-})
-
-api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  desc = "Set .conf.local filetype to conf",
-  pattern = { "*.conf.local" },
-  callback = function()
-    vim.api.nvim_command("set filetype=conf")
   end,
 })
 
@@ -94,7 +70,7 @@ api.nvim_create_autocmd("TextYankPost", {
 
 -- remember last position in file/buffer on nvim close and reopen
 api.nvim_create_autocmd("BufReadPost", {
-  desc = "go to last loc when opening a buffer",
+  desc = "Open buffer at the last location in file before last Nvim close",
   callback = function()
     local mark = vim.api.nvim_buf_get_mark(0, '"')
     local lcount = vim.api.nvim_buf_line_count(0)
