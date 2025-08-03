76aef97d6bb07654ac861eb4c9a3cddafd777794
Denis Iskandarov <d.iskandarov@gmail.com>
2025-08-03 18:51:49 +0400

    comments

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,4 +1,7 @@
 -- https://github.com/ray-x/go.nvim
+-- using friendly-snippets from VSCode for LuaSnip
+--   https://github.com/rafamadriz/friendly-snippets/blob/main/snippets/go.json
+-- ray-x/go.nvim snippets: https://github.com/ray-x/go.nvim/blob/master/lua/snips/go.lua
 return {
   "ray-x/go.nvim",
   dependencies = {
@@ -11,8 +14,11 @@ return {
   build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   config = function()
     require("go").setup({
+      -- we use nvim-lspconfig for completion and LSP configuration
       lsp_cfg = false,
+      -- nvim-cmd + luasnip + friendly-snippets for snippets
       luasnip = false,
+      -- dignostics using native nvim and lspconfig
       diagnostic = false,
       lsp_inlay_hints = {
         -- configured in `neovim/nvim-lspconfig` (lsp.lua)
