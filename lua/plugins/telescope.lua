2fe569a52c486f91771bbbc9106417dabb34a538
Denis Iskandarov <d.iskandarov@gmail.com>
2025-09-21 21:44:41 +0400

    telescope switch to master branch

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,9 @@
 -- https://github.com/nvim-telescope/telescope-fzf-native.nvim
 return {
   "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
+  -- branch "0.1.x" is deprecated https://github.com/nvim-telescope/telescope.nvim/issues/3487
+  branch = "master",
+  version = false,
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope-ui-select.nvim",
