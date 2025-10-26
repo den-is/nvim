dc1d1fc9e93f662ca761bbbad31cff68b96f12bd
Denis Iskandarov <d.iskandarov@gmail.com>
2025-10-26 22:50:29 +0400

    add comment

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -6,6 +6,7 @@
 --     Old text                    Command         New text
 -- --------------------------------------------------------------------------------
 --     surr*ound_words             ysiw)           (surround_words)
+--     surr*ound_words             ysiw(           ( surround_words )
 --     *make strings               ys$"            "make strings"
 --     [delete ar*ound me!]        ds]             delete around me!
 --     remove <b>HTML t*ags</b>    dst             remove HTML tags
