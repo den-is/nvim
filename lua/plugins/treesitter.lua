caf83349428baf20ad0865d5b6f05df817e6b2d3
Denis Iskandarov <d.iskandarov@gmail.com>
2025-10-26 17:26:44 +0400

    merge treesh.lua in treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -254,4 +254,23 @@ return {
       multiline_threshold = 2,
     },
   },
+  -- https://github.com/Wansmer/treesj
+  -- Neovim plugin for splitting/joining blocks of code like arrays, hashes,
+  -- statements, objects, dictionaries, etc
+  {
+    {
+      "Wansmer/treesj",
+      keys = {
+        { "<leader>mS", "", desc = "TreeSJ" },
+        { "<leader>mSt", "<CMD>TSJToggle<CR>", desc = "Toggle" },
+        { "<leader>mSj", "<CMD>TSJJoin<CR>", desc = "Join" },
+        { "<leader>mSs", "<CMD>TSJSplit<CR>", desc = "Split" },
+      },
+      dependencies = { "nvim-treesitter/nvim-treesitter" },
+      opts = {
+        use_default_keymaps = false,
+      },
+      config = true,
+    },
+  },
 }
