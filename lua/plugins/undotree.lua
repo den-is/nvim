3adb6eea018fc7539221b2ba0da4525627d916bc
Denis Iskandarov <d.iskandarov@gmail.com>
2025-10-26 14:12:48 +0400

    Update comments

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,9 +1,10 @@
 -- https://github.com/mbbill/undotree
+-- replaced by Snacks.picker.undo()
 return {
   "mbbill/undotree",
   enabled = false,
   event = { "BufReadPre", "BufNewFile" },
   config = function()
-    vim.keymap.set("n", "<leader>ut", vim.cmd.UndotreeToggle, { desc = "Open Undo Tree" })
+    vim.keymap.set("n", "<leader>ut", vim.cmd.UndotreeToggle, { desc = "Undo Tree" })
   end,
 }
