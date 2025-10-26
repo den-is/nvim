2de7696685801641e5cf2ca650a85991aaf42484
Denis Iskandarov <d.iskandarov@gmail.com>
2025-10-26 14:07:24 +0400

    and snacks* ft to autopairs disable

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -10,7 +10,15 @@ return {
   config = function()
     local autopairs = require("nvim-autopairs")
     autopairs.setup({
-      disable_filetype = { "TelescopePrompt", "alpha", "dashboard", "spectre_panel", "snacks_picker_input" },
+      disable_filetype = {
+        "TelescopePrompt",
+        "alpha",
+        "dashboard",
+        "spectre_panel",
+        "snacks_dashboard",
+        "snacks_input",
+        "snacks_picker_input",
+      },
       -- enable_check_bracket_line = false, -- Don't add pairs if it already has a close pair in the same line
       -- ignored_next_char = "[%w%.]", -- will ignore alphanumeric and `.` symbol
       check_ts = true, -- use treesitter to check for a pair.
