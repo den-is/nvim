6d6d26144d6ac164b1767d7bb9d2cb878f3b81aa
Denis Iskandarov <d.iskandarov@gmail.com>
2025-12-04 13:51:19 +0400

    move macos Lua 5.1 loading to init

--- init.lua
+++ init.lua
@@ -2,3 +2,14 @@ require("config.options")
 require("config.autocmds")
 require("config.keymaps")
 require("config.lazy")
+
+-- Hack for MacOS brew installed lua/luarocks
+-- required for https://github.com/CopilotC-Nvim/CopilotChat.nvim
+-- luarocks path --lr-cpath --lua-version 5.1
+-- :lua print(package.cpath)
+-- luarocks install --lua-version 5.1 tiktoken_core
+-- luarocks list --lua-version 5.1 tiktoken_core
+
+if vim.uv.os_uname().sysname == "Darwin" then
+  package.cpath = package.cpath .. ";" .. os.getenv("HOME") .. "/.luarocks/lib/lua/5.1/?.so"
+end
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,13 +2,6 @@
 -- :echo &runtimepath
 -- :new | put =split(&runtimepath, ',')
 
--- required for https://github.com/CopilotC-Nvim/CopilotChat.nvim
--- luarocks path --lr-cpath --lua-version 5.1
--- :lua print(package.cpath)
--- luarocks install --lua-version 5.1 tiktoken_core
--- luarocks list --lua-version 5.1 tiktoken_core
-package.cpath = package.cpath .. ";" .. os.getenv("HOME") .. "/.luarocks/lib/lua/5.1/?.so"
-
 -- UI -------------------------------------------------------------------------
 
 local env = vim.env
