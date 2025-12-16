04e059904db6d054114a967773f927dffe163868
Denis Iskandarov <d.iskandarov@gmail.com>
2025-12-16 11:56:37 +0400

    go disable dap_debug

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -15,6 +15,7 @@ return {
     build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
     config = function()
       require("go").setup({
+        dap_debug = false,
         -- we use nvim-lspconfig for completion and LSP configuration
         lsp_cfg = false,
         -- nvim-cmd + luasnip + friendly-snippets for snippets
