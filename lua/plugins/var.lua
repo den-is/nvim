53d108aac299b928bfbf7ee2ba2eed348ae5bdf1
Denis Iskandarov <d.iskandarov@gmail.com>
2026-09-19 14:17:33 +0400

    fix nvim-colorizer trigger events

--- lua/plugins/var.lua
+++ lua/plugins/var.lua
@@ -2,7 +2,7 @@ return {
   -- https://github.com/norcalli/nvim-colorizer.lua
   {
     "norcalli/nvim-colorizer.lua",
-    lazy = true,
+    event = { "BufReadPre", "BufNewFile" },
     config = function()
       require("colorizer").setup()
     end,
