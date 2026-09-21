5dfe2a6b3a03f7055cd8fcf1f8879a4af9e45d1d
Denis Iskandarov <d.iskandarov@gmail.com>
2026-09-21 21:04:40 +0400

    optimize ts.install(ensure_installed) which already checks installed languages

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -65,17 +65,6 @@ return {
         "zig",
         "zsh",
       }
-      -- local installed = ts_cfg.get_installed()
-      -- local to_install = vim
-      --   .iter(ensure_installed)
-      --   :filter(function(parser)
-      --     return not vim.tbl_contains(installed, parser)
-      --   end)
-      --   :totable()
-      --
-      -- if #to_install > 0 then
-      --   ts.install(to_install)
-      -- end
       ts.install(ensure_installed)
 
       local ignore_filetype = {
